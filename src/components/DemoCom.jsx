7d3754515eb55a02a6d9d8f122b60353bafc60a6
oliver <youremail@example.com>
2025-04-10 17:19:54 +0800

    add accordion

--- src/components/DemoCom.jsx
+++ src/components/DemoCom.jsx
@@ -1,8 +1,8 @@
 export default function Profile() {
     return (
       <img
-        src="https://i.imgur.com/MK3eW3Am.jpg"
-        alt="Katherine Johnsondd"
+        src="https://download.schneider-electric.com/files?p_Doc_Ref=SPD_STOS-7RUNCR_B_V&p_File_Type=rendition_369_jpg&default_image=DefaultProductImage.png"
+        alt="ups"
       />
     )
   }
