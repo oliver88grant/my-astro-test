32b79b859dcef1e129803693d7d137fdc317c100
anthony <luxingchaode@gmail.com>
2025-06-23 20:55:57 +0800

    update inquiry logic

--- src/config/const.js
+++
@@ -1,4 +0,0 @@
-export const inquiry_base_url = 'https://nestjs.50d.top';
-export const inquiry_url = '/customer-inquiries/inquiryAndEmail';
-export const company_name = 'rhimopower';
-export const to_email = 'rhimopower@gmail.com';
